67e190e711ecf8c36268dd76ed5f3512f8abbb33
Nora Ekramy <nora.ekramy11@gmail.com>
2020-11-12 20:03:33 +0200

    Add files via upload

---
+++ newlol.js
@@ -0,0 +1,380 @@
+<script>
+
+var poop1 = document.getElementById("pop1");
+		   var poop2 = document.getElementById("pop2");
+		   var poop3 = document.getElementById("pop3");
+		   var poop4 = document.getElementById("pop4");
+		   var poop5 = document.getElementById("pop5");
+		   var poop6 = document.getElementById("pop6");
+		   var poop7 = document.getElementById("pop7");
+		   var poop8 = document.getElementById("pop8");
+		   var betengan=0;
+		   var betengan1=0;
+		   
+		   
+poop1.onclick = function( betengan)
+{
+
+ betengan=1;
+ if(betengan == 1)
+{
+	changecolor('sienna');
+	
+}
+else if(betengan == 2)
+{
+	changecolor('purple');
+}
+
+else if(betengan ==3)
+{
+	changecolor('dimgray');
+}
+
+else if(betengan== 4)
+{
+	changecolor('darkgreen');
+}
+
+
+
+
+if(betengan1 == 1)
+{
+	changecolor('red');
+}
+else if(betengan1 == 2)
+{
+	changecolor('white');
+}
+
+else if(betengan1 ==3)
+{
+	changecolor('blue');
+}
+
+else if(betengan1 == 4)
+{
+	changecolor('black');
+}
+}
+poop2.onclick = function(betengan)
+{
+ betengan=2;
+ if(betengan == 1)
+{
+	changecolor('sienna');
+}
+else if(betengan == 2)
+{
+	changecolor('purple');
+}
+
+else if(betengan ==3)
+{
+	changecolor('dimgray');
+}
+
+else if(betengan== 4)
+{
+	changecolor('darkgreen');
+}
+
+
+
+
+if(betengan1 == 1)
+{
+	changecolor('red');
+}
+else if(betengan1 == 2)
+{
+	changecolor('white');
+}
+
+else if(betengan1 ==3)
+{
+	changecolor('blue');
+}
+
+else if(betengan1 == 4)
+{
+	changecolor('black');
+}
+
+}
+poop3.onclick = function()
+{
+betengan=3;
+if(betengan == 1)
+{
+	changecolor('sienna');
+}
+else if(betengan == 2)
+{
+	changecolor('purple');
+}
+
+else if(betengan ==3)
+{
+	changecolor('dimgray');
+}
+
+else if(betengan== 4)
+{
+	changecolor('darkgreen');
+}
+
+
+
+
+if(betengan1 == 1)
+{
+	changecolor('red');
+}
+else if(betengan1 == 2)
+{
+	changecolor('white');
+}
+
+else if(betengan1 ==3)
+{
+	changecolor('blue');
+}
+
+else if(betengan1 == 4)
+{
+	changecolor('black');
+}
+}
+poop4.onclick = function()
+{
+
+betengan=4;
+if(betengan == 1)
+{
+	changecolor('sienna');
+}
+else if(betengan == 2)
+{
+	changecolor('purple');
+}
+
+else if(betengan ==3)
+{
+	changecolor('dimgray');
+}
+
+else if(betengan== 4)
+{
+	changecolor('darkgreen');
+}
+
+
+
+
+if(betengan1 == 1)
+{
+	changecolor('red');
+}
+else if(betengan1 == 2)
+{
+	changecolor('white');
+}
+
+else if(betengan1 ==3)
+{
+	changecolor('blue');
+}
+
+else if(betengan1 == 4)
+{
+	changecolor('black');
+}
+}
+poop5.onclick = function()
+{
+
+betengan1=1;
+if(betengan == 1)
+{
+	changecolor('sienna');
+}
+else if(betengan == 2)
+{
+	changecolor('purple');
+}
+
+else if(betengan ==3)
+{
+	changecolor('dimgray');
+}
+
+else if(betengan== 4)
+{
+	changecolor('darkgreen');
+}
+
+
+
+
+if(betengan1 == 1)
+{
+	changecolor('red');
+}
+else if(betengan1 == 2)
+{
+	changecolor('white');
+}
+
+else if(betengan1 ==3)
+{
+	changecolor('blue');
+}
+
+else if(betengan1 == 4)
+{
+	changecolor('black');
+}
+}
+poop6.onclick = function()
+{
+betengan1=2;
+if(betengan == 1)
+{
+	changecolor('sienna');
+}
+else if(betengan == 2)
+{
+	changecolor('purple');
+}
+
+else if(betengan ==3)
+{
+	changecolor('dimgray');
+}
+
+else if(betengan== 4)
+{
+	changecolor('darkgreen');
+}
+
+
+
+
+if(betengan1 == 1)
+{
+	changecolor('red');
+}
+else if(betengan1 == 2)
+{
+	changecolor('white');
+}
+
+else if(betengan1 ==3)
+{
+	changecolor('blue');
+}
+
+else if(betengan1 == 4)
+{
+	changecolor('black');
+}
+
+}
+poop7.onclick = function()
+{
+betengan1=3;
+if(betengan == 1)
+{
+	changecolor('sienna');
+}
+else if(betengan == 2)
+{
+	changecolor('purple');
+}
+
+else if(betengan ==3)
+{
+	changecolor('dimgray');
+}
+
+else if(betengan== 4)
+{
+	changecolor('darkgreen');
+}
+
+
+
+
+if(betengan1 == 1)
+{
+	changecolor('red');
+}
+else if(betengan1 == 2)
+{
+	changecolor('white');
+}
+
+else if(betengan1 ==3)
+{
+	changecolor('blue');
+}
+
+else if(betengan1 == 4)
+{
+	changecolor('black');
+}
+
+}
+poop8.onclick = function()
+{
+betengan1=4;
+if(betengan == 1)
+{
+	changecolor('sienna');
+}
+else if(betengan == 2)
+{
+	changecolor('purple');
+}
+
+else if(betengan ==3)
+{
+	changecolor('dimgray');
+}
+
+else if(betengan== 4)
+{
+	changecolor('darkgreen');
+}
+
+
+
+
+if(betengan1 == 1)
+{
+	changecolor('red');
+}
+else if(betengan1 == 2)
+{
+	changecolor('white');
+}
+
+else if(betengan1 ==3)
+{
+	changecolor('blue');
+}
+
+else if(betengan1 == 4)
+{
+	changecolor('black');
+}
+
+}
+		  
+
+		  
+
+
+</script>
